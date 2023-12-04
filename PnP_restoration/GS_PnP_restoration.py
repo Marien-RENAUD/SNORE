Correct standard weights for Average_PnP

PnP_restoration/GS_PnP_restoration.py
@@ -108,7 +108,6 @@ class PnP_restoration():
         else:
             raise ValueError('noise model not implemented')
         return x - stepsize*grad, grad
-
         
     def A(self,y):
         '''
@@ -198,7 +197,7 @@ class PnP_restoration():
             y_list, z_list, x_list, Dg_list, psnr_tab, ssim_tab, lpips_tab, g_list, f_list, Df_list, F_list, Psi_list = [], [], [], [], [], [], [], [], [], [], [], []
 
         # initalize parameters
-        if (self.hparams.opt_alg == "PnP_Prox" or self.hparams.opt_alg == "PnP_GD"):
+        if (self.hparams.opt_alg == "PnP_Prox" or self.hparams.opt_alg == "PnP_GD" or self.hparams.opt_alg == "Data_GD"):
             if self.hparams.stepsize is None:
                 self.stepsize = 1 / self.lamb
             else:
@@ -325,6 +324,18 @@ class PnP_restoration():
             #     use_backtracking = self.hparams.use_backtracking
             #     early_stopping = self.hparams.early_stopping
 
+            if self.hparams.opt_alg == "Data_GD":
+                z = x_old
+                # Data-fidelity step
+                x = self.data_fidelity_prox_step(z, img_tensor, self.stepsize)
+                y = z # output image is the output of the denoising step
+                if self.hparams.use_hard_constraint:
+                    x = torch.clamp(x,0,1)
+                # Calculate Objective
+                g=torch.tensor(0).float()
+                Dg=torch.tensor(0).float()
+                f, F = self.calculate_F(x, img_tensor, g=g)
+
             if self.hparams.opt_alg == "PnP_Prox" or self.hparams.opt_alg == "PnP_GD":
                 # Gradient of the regularization term
                 _,g,Dg = self.denoise(x_old, self.sigma_denoiser)
@@ -341,32 +352,34 @@ class PnP_restoration():
                 # Calculate Objective
                 f, F = self.calculate_F(x, img_tensor, g=g)
 
+                
             if self.hparams.opt_alg == "Average_PnP" or self.hparams.opt_alg == "Average_PnP_Prox":
                 x_old = x
                 if i % 50 == 0 and i < self.maxitr//2:
-                    std_i =  self.std_0 * (1 - i / (self.maxitr//2)) + self.std_end * (i / (self.maxitr//2))
-                    # std_i = std
-                    lamb_i = self.lamb_0 * (1 - i / (self.maxitr//2)) + self.lamb_end * (i / (self.maxitr//2))
-                    # lamb_i = lamb
-                if i >= self.hparams.maxitr//2:
-                    std_i = self.std_end
-                    lamb_i = self.lamb_end
+                    self.std =  self.std_0 * (1 - i / (self.maxitr//2)) + self.std_end * (i / (self.maxitr//2))
+                    self.lamb = self.lamb_0 * (1 - i / (self.maxitr//2)) + self.lamb_end * (i / (self.maxitr//2))
+                if i >= self.maxitr//2:
+                    self.std = self.std_end
+                    self.lamb = self.lamb_end
 
                 # Regularization term
-                noise = torch.normal(torch.zeros(*x_old.size()).to(self.device), std = std_i*torch.ones(*x_old.size()).to(self.device))
+                noise = torch.normal(torch.zeros(*x_old.size()).to(self.device), std = self.std*torch.ones(*x_old.size()).to(self.device))
                 x_old_noise = x_old + noise
-                _,g,Dg = self.denoise(x_old_noise, std_i)
+                _,g,Dg = self.denoise(x_old_noise, self.std)
                 # Total-Gradient step
-                z = x_old - self.stepsize * lamb_i * Dg 
+                z = x_old - self.stepsize * self.lamb * Dg 
                 if self.hparams.opt_alg == "Average_PnP":
                     x = z - self.stepsize * self.data_fidelity_grad(x_old, img_tensor)
                 if self.hparams.opt_alg == "Average_PnP_Prox":
                     x = self.data_fidelity_prox_step(z, img_tensor, self.stepsize)
+                # print("reg = ",torch.sum(torch.abs(self.lamb * Dg)))
+                # print("data = ",torch.sum(torch.abs(self.data_fidelity_grad(x_old, img_tensor))))
+                
                 # Hard constraint
                 if self.hparams.use_hard_constraint:
                     x = torch.clamp(x,0,1)
                 # Calculate Objective
-                f, F = self.calculate_F(x, img_tensor, lamb_i, g=g)
+                f, F = self.calculate_F(x, img_tensor, g=g)
 
                 y = x # output image is the output of the denoising step
                 z = x # To be modified, for no errors in the followinf code            
@@ -381,6 +394,9 @@ class PnP_restoration():
                     print('backtracking : stepsize =', self.stepsize, 'diff_F=', diff_F)
                 else :
                     self.backtracking_check = True
+                # if (abs(self.stepsize) < 1e-7):
+                #     print(f'Convergence reached at iteration {i}')
+                #     break
 
             if self.backtracking_check : # if the backtracking condition is satisfied
                 # Logging
@@ -411,7 +427,7 @@ class PnP_restoration():
                 # check decrease of data_fidelity 
                 if self.hparams.early_stopping : 
                     if self.hparams.crit_conv == 'cost':
-                        if abs(diff_F)/abs(F) < self.hparams.thres_conv:
+                        if (abs(diff_F)/abs(F) < self.hparams.thres_conv):
                             print(f'Convergence reached at iteration {i}')
                             break
                     elif self.hparams.crit_conv == 'residual':
@@ -604,5 +620,5 @@ class PnP_restoration():
         parser.add_argument('--weight_Dg', type=float, default=1.)
         parser.add_argument('--n_init', type=int, default=10)
         parser.add_argument('--act_mode_denoiser', type=str, default='E')
-        parser.add_argument('--opt_alg', dest='opt_alg', choices=['Average_PnP', 'Average_PnP_Prox', 'PnP_Prox', 'PnP_GD', 'PnP_AGD', 'PnP_SGD'], help='Specify optimization algorithm')
+        parser.add_argument('--opt_alg', dest='opt_alg', choices=['Average_PnP', 'Data_GD', 'Average_PnP_Prox', 'PnP_Prox', 'PnP_GD', 'PnP_AGD', 'PnP_SGD'], help='Specify optimization algorithm')
         return parser

PnP_restoration/deblur.py
@@ -97,29 +97,40 @@ def deblur():
         if hparams.extract_curves:
             PnP_module.initialize_curves()
 
-        if PnP_module.hparams.opt_alg == 'PnP_Prox' or PnP_module.hparams.opt_alg == 'PnP_GD':
+        PnP_module.lamb, PnP_module.lamb_0, PnP_module.lamb_end, PnP_module.maxitr, PnP_module.std_0, PnP_module.std_end, PnP_module.stepsize = None, None, None, None, None, None, None
+
+        if PnP_module.hparams.opt_alg == 'PnP_Prox' or PnP_module.hparams.opt_alg == 'PnP_GD' or PnP_module.hparams.opt_alg == 'Data_GD':
             PnP_module.lamb, PnP_module.sigma_denoiser, PnP_module.maxitr, PnP_module.thres_conv = get_gaussian_noise_parameters(hparams.noise_level_img, PnP_module.hparams, k_index=k_index, degradation_mode='deblur')
-            print(PnP_module.lamb)
-            print(PnP_module.sigma_denoiser)
-            print(PnP_module.thres_conv)
             print('GS-DRUNET deblurring with image sigma:{:.3f}, model sigma:{:.3f}, lamb:{:.3f} \n'.format(PnP_module.hparams.noise_level_img, PnP_module.sigma_denoiser, PnP_module.lamb))
 
         if PnP_module.hparams.opt_alg == 'Average_PnP' or PnP_module.hparams.opt_alg == 'Average_PnP_Prox':
             PnP_module.std_0 = 1.8 * hparams.noise_level_img /255.
-            PnP_module.std_end = 1.8 /255.
-            PnP_module.stepsize = 1.
+            PnP_module.std_end = 1.8 / 255.
+            PnP_module.stepsize = 0.1
             if hparams.noise_level_img == 1:
-                if PnP_module.hparams.lamb == None:
-                    PnP_module.lamb = PnP_module.lamb_end = PnP_module.lamb_0 = 1.
+                if PnP_module.hparams.lamb == None and PnP_module.hparams.lamb_end == None:
+                    PnP_module.lamb = PnP_module.lamb_end = PnP_module.lamb_0 = 3.
+                if not(PnP_module.hparams.lamb_end == None):
+                    PnP_module.lamb = PnP_module.lamb_0 = 1.
+                    PnP_module.lamb_end = PnP_module.hparams.lamb_end
+                if PnP_module.hparams.lamb_end == None and not(PnP_module.hparams.lamb == None):
+                    PnP_module.lamb = PnP_module.lamb_0 = PnP_module.lamb_end = PnP_module.hparams.lamb
+                if PnP_module.hparams.maxitr == None:
+                    PnP_module.maxitr = 200
                 else:
-                    PnP_module.lamb_end = PnP_module.lamb_0 = PnP_module.hparams.lamb
-                PnP_module.hparams.maxitr = 200
+                    PnP_module.maxitr = PnP_module.hparams.maxitr
             if hparams.noise_level_img == 10:
-                if PnP_module.hparams.lamb == None:
-                    PnP_module.lamb = PnP_module.lamb_end = PnP_module.lamb_0 = .3
+                if (PnP_module.hparams.lamb == None) and (PnP_module.hparams.lamb_end == None):
+                    PnP_module.lamb = PnP_module.lamb_end = PnP_module.lamb_0 = 3.
+                if not(PnP_module.hparams.lamb_end == None):
+                    PnP_module.lamb_0 = .3
+                    PnP_module.lamb_end = PnP_module.hparams.lamb_end
+                if PnP_module.hparams.lamb_end == None and not(PnP_module.hparams.lamb == None):
+                    PnP_module.lamb_0 = PnP_module.lamb_end = PnP_module.hparams.lamb
+                if PnP_module.hparams.maxitr == None:
+                    PnP_module.maxitr = 600
                 else:
-                    PnP_module.lamb_end = PnP_module.lamb_0 = PnP_module.hparams.lamb
-                PnP_module.hparams.maxitr = 600
+                    PnP_module.maxitr = PnP_module.hparams.maxitr
 
         if hparams.extract_images or hparams.extract_curves or hparams.print_each_step:
             # exp_out_path = create_out_dir(hparams.degradation_mode, hparams.dataset_name)
@@ -140,6 +151,10 @@ def deblur():
                 os.mkdir(exp_out_path)
             if PnP_module.hparams.lamb != None:
                 exp_out_path = os.path.join(exp_out_path, "lamb_"+str(PnP_module.hparams.lamb))
+                if not os.path.exists(exp_out_path):
+                    os.mkdir(exp_out_path)
+            if PnP_module.hparams.no_data_term == True:
+                exp_out_path = os.path.join(exp_out_path, "no_data_term")
                 if not os.path.exists(exp_out_path):
                     os.mkdir(exp_out_path)
 
@@ -233,6 +248,14 @@ def deblur():
                         'PSNR_output' : output_psnr,
                         'LPIPS_output' : output_lpips,
                         'kernel' : k,
+                        'lamb' : PnP_module.lamb,
+                        'lamb_0' : PnP_module.lamb_0,
+                        'lamb_end' : PnP_module.lamb_end,
+                        'maxitr' : PnP_module.maxitr,
+                        'std_0' : PnP_module.std_0,
+                        'std_end' : PnP_module.std_end,
+                        'stepsize' : PnP_module.stepsize,
+                        'opt_alg': PnP_module.hparams.opt_alg,
                     }
                 np.save(os.path.join(exp_out_path, 'dict_' + str(i) + '_results'), dict)
 