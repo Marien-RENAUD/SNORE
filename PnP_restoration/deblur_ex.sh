0c798ca42d9d4c6f85cbe1a7a55b0bca41748f93
marien.renaud@laposte.net <mrenaud@sirocco24.plafrim.cluster>
2023-12-19 14:19:16 +0100

    Update bash file

--- PnP_restoration/deblur_ex.sh
+++
@@ -1,9 +0,0 @@
-for i in 1 2 3 4 5 6 7 8 9 10 
-do
-# 11 12 13 14 15 16 17 18 19 20 21 22
-# #i is the number of layers of the denoiser can be between an integer between 1 and 17
-    python deblur.py --annealing_number $i --extract_images --extract_curves --gpu_number 0 --kernel_indexes 0 --noise_level_img 10 --dataset_name "set4c" --opt_alg "Average_PnP"
-done
-
-# for noise_level_img = 1./255. / take : std_0 = std_end = 1.8 /255. // lamb_end = lamb_0 = 1. // tau = 1 // maxitr = 200
-# for noise_level_img = 10./255. / take : std_0 = 18./255. // std_end = 1.8 /255. // lamb_end = lamb_0 = .3 // tau = 1 // maxitr = 600
--- PnP_restoration/ex_deblur_2.sh
+++
@@ -1,5 +0,0 @@
-for i in 0.25 0.3 0.35 0.4 0.45 0.5
-# #i is the number of layers of the denoiser can be between an integer between 1 and 17
-do
-    python deblur.py --extract_images --lamb $i --extract_curves --gpu_number 1 --kernel_indexes 0 --noise_level_img 10 --dataset_name "set1c" --opt_alg "PnP_GD"
-done
